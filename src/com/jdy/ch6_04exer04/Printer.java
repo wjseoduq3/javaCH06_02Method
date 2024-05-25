e2ca823fdb9efe109a31f57f27237c6dd20c6982
wjseoduq3 <dragon65@nate.com>
2024-05-25 14:36:23 +0900

    5'th commit

---
+++ src/com/jdy/ch6_04exer04/Printer.java
@@ -0,0 +1,20 @@
+package com.jdy.ch6_04exer04;
+
+public class Printer {
+	void println(int a) {
+		System.out.println(a);
+	}
+	
+	void println(double a) {
+		System.out.println(a);
+	}
+	
+	void println(boolean a) {
+		System.out.println(a);
+	}
+	
+	void println(String a) {
+		System.out.println(a);
+	}
+}
+
---
+++ src/com/jdy/ch6_04exer04/PrinterExample.java
@@ -0,0 +1,14 @@
+package com.jdy.ch6_04exer04;
+
+public class PrinterExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Printer printer = new Printer();
+		
+		printer.println(10);
+		printer.println(true);
+		printer.println(3.14);
+		printer.println("goodday");
+	}
+}
