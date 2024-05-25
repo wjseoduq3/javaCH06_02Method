f457531f5fe18fbd4206acbb212a1f4713a0745d
wjseoduq3 <dragon65@nate.com>
2024-05-25 21:00:02 +0900

    6'th commit

---
+++ src/com/jdy/ch6_05static/CarExample.java
@@ -0,0 +1,14 @@
+package com.jdy.ch6_05static;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car myCar = new Car("Porshe");
+		Car yourCar = new Car("Benz");
+		
+		myCar.run();
+		yourCar.run();
+	}
+
+}
