b0c7cb3c899fccaac537f1c13061579865369963
wjseoduq3 <dragon65@nate.com>
2024-05-26 10:44:55 +0900

    7'th commit

---
+++ src/com/jdy/ch6_05static/Calculator.java
@@ -0,0 +1,13 @@
+package com.jdy.ch6_05static;
+
+public class Calculator {
+	public static double pi = 3.141596254;
+	
+	public static int plus(int x, int y) {
+		return x+y;
+	}
+	
+	public static int minus(int x, int y) {
+		return x-y;
+	}
+}
---
+++ src/com/jdy/ch6_05static/CalculatorExample.java
@@ -0,0 +1,17 @@
+package com.jdy.ch6_05static;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		double result3 = (10*10)*Calculator.pi;
+		
+		int result1 = Calculator.plus(5, 6);
+		int result2 = Calculator.minus(5, 6);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+	}
+}
